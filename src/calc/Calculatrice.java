b3013127114bdba3a6738e5a3a5484fd0b54777e
mwarcko <marcantoinefournier145@gmail.com>
2019-01-14 12:59:17 +0100

    ajout des fonctions addition soustraction division et multiplication + splitter

--- src/calc/Calculatrice.java
+++ src/calc/Calculatrice.java
@@ -12,8 +12,9 @@ public class Calculatrice {
 		String regSign = "[*\\-+\\/]";
 		String regNum = "[0-9]+";
 
-		String[] splittedNum = calcul.split(regSign);
-		String[] splittedSign = calcul.split(regNum);
+		String[] splittedNum = splitter(calcul, regSign);
+		String[] splittedSign = splitter(calcul, regNum);
+
 		for (int i = 0; i < splittedNum.length; i++) {
 			System.out.println("numéro [" + i + "]" + splittedNum[i]);
 		}
@@ -21,6 +22,27 @@ public class Calculatrice {
 		for (int i = 0; i < splittedSign.length; i++) {
 			System.out.println("signe: [" + i + "]" + splittedSign[i]);
 		}
+
+		// Reconnaissance des signes
 	}
 
+	public Integer addition(Integer a, Integer b) {
+		return a + b;
+	}
+
+	public Integer division(Integer a, Integer b) {
+		return a / b;
+	}
+
+	public Integer multiplication(Integer a, Integer b) {
+		return a * b;
+	}
+
+	public Integer soustraction(Integer a, Integer b) {
+		return a - b;
+	}
+
+	public static String[] splitter(String toSplit, String reg) {
+		return toSplit.split(reg);
+	}
 }
